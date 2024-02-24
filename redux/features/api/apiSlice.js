0dab5b7eca11659de21675c478c8b86fec1f2627
fai-sas <craftedbyfaisal@gmail.com>
2024-02-24 23:12:52 +0600

    apiSlice updated

--- redux/features/api/apiSlice.js
+++ redux/features/api/apiSlice.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3000/api',
+    // baseUrl: 'http://localhost:3000/api',
+    baseUrl: 'https://bookster-alpha.vercel.app/api',
   }),
   tagTypes: [],
   endpoints: (builder) => ({}),
