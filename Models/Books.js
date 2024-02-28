5078b60ca9359996e9ee73f1dcbb9e3d3674db75
fai-sas <craftedbyfaisal@gmail.com>
2024-02-28 19:58:59 +0600

    book model created

---
+++ Models/Books.js
@@ -0,0 +1,88 @@
+import mongoose from 'mongoose'
+
+const Books = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide  book name'],
+    },
+    author: {
+      type: String,
+      required: [true, 'Please provide  book name'],
+    },
+    category: {
+      type: String,
+      required: [true, 'Please provide  category name'],
+      enum: [
+        'Fiction',
+        'Non-Fiction',
+        'Science Fiction',
+        'Fantasy',
+        'Mystery',
+        'Thriller',
+        'Romance',
+        'Historical Fiction',
+        'Biography',
+        'Self-Help',
+        'Children',
+      ],
+    },
+    description: {
+      type: String,
+      required: [true, 'Please provide description '],
+    },
+    isbn: {
+      type: String,
+      required: [true, 'Please provide isbn number'],
+    },
+    edition: {
+      type: Number,
+      default: 1,
+    },
+    language: {
+      type: String,
+      trim: true,
+    },
+    format: {
+      type: String,
+      required: [
+        true,
+        'Please provide format (HardCover/Paperback/Audiobook/e-book) ',
+      ],
+      enum: ['Hardcover', 'Paperback', 'Audiobook', 'E-book'],
+    },
+    pages: {
+      type: Number,
+      min: 1,
+    },
+    stock: {
+      type: Number,
+      required: [true, 'Please provide stock quantity'],
+      default: 1,
+      min: 1,
+    },
+    price: {
+      type: Number,
+      required: [true, 'Please provide price'],
+      min: 0.01,
+    },
+    image: {
+      type: [String],
+      required: [true, 'Please provide book image'],
+    },
+    createdByUserId: {
+      type: String,
+      required: [true, 'Please provide user Id'],
+    },
+    createdByUserName: {
+      type: String,
+      required: [true, 'Please provide user name'],
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+)
+
+export default mongoose.models.Books || mongoose.model('Books', Books)
