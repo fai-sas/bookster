892feab7139643823be41d3484dccfdd71699fed
fai-sas <craftedbyfaisal@gmail.com>
2024-02-28 20:24:08 +0600

    end point created to edit books

--- app/api/books/[id]/route.js
+++ app/api/books/[id]/route.js
@@ -26,3 +26,29 @@ export async function GET(request, { params }) {
     )
   }
 }
+
+export async function PUT(request, { params }) {
+  try {
+    const { id } = params
+    const updatedBook = await request.json()
+
+    await connectDB()
+
+    const result = await Books.findByIdAndUpdate(id, { ...updatedBook })
+
+    if (!result) {
+      return NextResponse.json(
+        { message: `No book found with id : ${id}` },
+        { status: 404 }
+      )
+    }
+
+    return NextResponse.json({ message: 'Book updated' }, { status: 200 })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json(
+      { message: `Internal Server Error: ${error.message}` },
+      { status: 500 }
+    )
+  }
+}
