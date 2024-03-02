f603b971588c97f7e13dddf0140af70ab97f45a5
fai-sas <craftedbyfaisal@gmail.com>
2024-03-02 22:25:28 +0600

    category wise books rendered

--- app/(dashboard)/categories/[id]/page.jsx
+++ app/(dashboard)/categories/[id]/page.jsx
@@ -1,11 +1,22 @@
 'use client'
 
+import BooksByAuthor from '@/components/BooksByAuthor'
+import { useGetBooksQuery } from '@/redux/features/books/booksApi'
 import { useGetSingleCategoryQuery } from '@/redux/features/categories/categoriesApi'
 
 const SingleCategoryPage = ({ params }) => {
   const { id } = params
-  const { data, isLoading } = useGetSingleCategoryQuery(id)
-  console.log(data)
+  const { data: categories, isLoading } = useGetSingleCategoryQuery(id)
+
+  const { data: books } = useGetBooksQuery()
+
+  const categoryName = categories?.name
+
+  const booksByCategory = books?.books
+
+  const matchingBooks = booksByCategory?.filter(
+    (book) => book?.category === categoryName
+  )
 
   if (isLoading) {
     return <h1 className='p-8 text-4xl font-bold'>Loading.........</h1>
@@ -13,7 +24,8 @@ const SingleCategoryPage = ({ params }) => {
 
   return (
     <>
-      <li className='p-2 text-4xl font-bold '>{data?.name}</li>
+      <li className='p-2 text-4xl font-bold '>{categories?.name}</li>
+      <BooksByAuthor matchingBooks={matchingBooks} />
     </>
   )
 }
